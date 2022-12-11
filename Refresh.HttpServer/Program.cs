8052bd65f9cc80a38430f54e855ec2f26d4410d0
jvyden <jvyden@jvyden.xyz>
2022-12-10 22:01:46 -0500

    Add sln files, socket listening

---
+++ Refresh.HttpServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Refresh.HttpServer;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            listener.Bind(new IPEndPoint(IPAddress.Loopback, 10060));
+            listener.Listen(2^7);
+
+            while (true)
+            {
+                Socket client = listener.Accept();
+                Console.WriteLine("Accepted");
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Failed to accept connection: " + e);
+        }
+    }
+}
