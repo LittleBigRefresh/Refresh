905860e21eada3a35e04df738be4c2a3758c0c58
jvyden <jvyden@jvyden.xyz>
2022-12-11 23:50:30 -0500

    Remove TestServer

--- Refresh.TestServer/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Refresh.HttpServer;
-using Refresh.TestServer.Endpoints;
-
-RefreshHttpServer server = new(new Uri("http://localhost:10060"));
-server.AddEndpoint<TestEndpoint>();
-await server.StartAndBlockAsync();
