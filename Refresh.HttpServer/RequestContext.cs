99dfaf269205298a2901d6b28ca2b1cccf38f852
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-12-14 22:37:28 -0800

    Add logger to `RequestContext` (#6)

--- Refresh.HttpServer/RequestContext.cs
+++ Refresh.HttpServer/RequestContext.cs
@@ -1,8 +1,10 @@
 using System.Net;
+using NotEnoughLogs;
 
 namespace Refresh.HttpServer;
 
 public struct RequestContext
 {
     public HttpListenerRequest Request;
+    public LoggerContainer<RefreshContext> Logger;
 }
