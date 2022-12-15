a86469bb3d06d9e37cd781de5240fee6cc897d4f
jvyden <jvyden@jvyden.xyz>
2022-12-14 21:54:06 -0500

    Re-implement 404 request logging

--- Refresh.GameServer/Startup.cs
+++ Refresh.GameServer/Startup.cs
@@ -10,4 +10,23 @@ server.UseAuthenticationProvider(new GameAuthenticationProvider());
 
 server.DiscoverEndpointsFromAssembly(Assembly.GetExecutingAssembly());
 
+#region Log unimplemented endpoints
+
+const string endpointFile = "unimplementedEndpoints.txt";
+if(!File.Exists(endpointFile)) File.WriteAllText(endpointFile, string.Empty);
+List<string> unimplementedEndpoints = File.ReadAllLines(endpointFile).ToList();
+
+server.NotFound += (sender, context) =>
+{
+    if (context.Request.Url == null) return;
+    Uri url = context.Request.Url;
+
+    if (unimplementedEndpoints.Any(e => e.Split('?')[0] == url.AbsolutePath)) return;
+
+    unimplementedEndpoints.Add(url.PathAndQuery);
+    File.WriteAllLines(endpointFile, unimplementedEndpoints);
+};
+
+#endregion
+
 await server.StartAndBlockAsync();
