3b577bd0aa15873016a6db83f982c506cc5f343c
jvyden <jvyden@jvyden.xyz>
2023-01-20 21:25:34 -0500

    Remove all refresh project files

--- Refresh.HttpServer/Database/IDatabaseProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Refresh.HttpServer.Database;
-
-public interface IDatabaseProvider<out TDatabaseContext> : IDisposable where TDatabaseContext : IDatabaseContext
-{
-    void Initialize();
-
-    TDatabaseContext GetContext();
-}
--- RefreshTests.HttpServerManual/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Refresh.HttpServer;
-using RefreshTests.HttpServer;
-
-RefreshConsole.AllocateConsole();
-
-RefreshHttpServer server = new("http://+:10060/");
-server.DiscoverEndpointsFromAssembly(typeof(ServerDependentTest).Assembly);
-
-await server.StartAndBlockAsync();
