0cb1dd2fbcfea62f7f3332752ce27f80563121ef
jvyden <jvyden@jvyden.xyz>
2023-05-04 19:09:12 -0400

    API endpoint for retrieving scores

---
+++ Refresh.GameServer/Endpoints/Api/LeaderboardApiEndpoints.cs
@@ -0,0 +1,22 @@
+using Bunkum.HttpServer;
+using Bunkum.HttpServer.Endpoints;
+using Refresh.GameServer.Database;
+using Refresh.GameServer.Types.Levels;
+using Refresh.GameServer.Types.UserData.Leaderboard;
+
+namespace Refresh.GameServer.Endpoints.Api;
+
+public class LeaderboardApiEndpoints : EndpointGroup
+{
+    [ApiEndpoint("scores/{id}")]
+    [Authentication(false)]
+    public List<GameSubmittedScore>? GetTopScoresForLevel(RequestContext context, GameDatabaseContext database, int? id)
+    {
+        if (id == null) return null;
+        
+        GameLevel? level = database.GetLevelById(id.Value);
+        if (level == null) return null;
+
+        return database.GetTopScoresForLevel(level).ToList();
+    }
+}
