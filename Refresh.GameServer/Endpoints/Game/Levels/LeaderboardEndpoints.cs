98c36580c1f27cef196ebfe1772d3594fdfd8406
jvyden <jvyden@jvyden.xyz>
2023-05-04 03:31:38 -0400

    Use nullable int in PlayLevel

--- Refresh.GameServer/Endpoints/Game/Levels/LeaderboardEndpoints.cs
+++ Refresh.GameServer/Endpoints/Game/Levels/LeaderboardEndpoints.cs
@@ -12,11 +12,11 @@ namespace Refresh.GameServer.Endpoints.Game.Levels;
 public class LeaderboardEndpoints : EndpointGroup
 {
     [GameEndpoint("play/user/{id}", ContentType.Xml, Method.Post)]
-    public Response PlayLevel(RequestContext context, GameUser user, GameDatabaseContext database, int id)
+    public Response PlayLevel(RequestContext context, GameUser user, GameDatabaseContext database, int? id)
     {
         if (id == null) return HttpStatusCode.BadRequest;
 
-        GameLevel? level = database.GetLevelById(id);
+        GameLevel? level = database.GetLevelById(id.Value);
         if (level == null) return HttpStatusCode.NotFound;
 
         bool result = database.PlayLevel(level, user);
