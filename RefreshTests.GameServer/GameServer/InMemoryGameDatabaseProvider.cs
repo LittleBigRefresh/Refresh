e361dd488096e0bcc23efb03c40730ed53da99c4
jvyden <jvyden@jvyden.xyz>
2023-05-08 18:57:06 -0400

    Set random database ID only once

--- RefreshTests.GameServer/GameServer/InMemoryGameDatabaseProvider.cs
+++ RefreshTests.GameServer/GameServer/InMemoryGameDatabaseProvider.cs
@@ -4,6 +4,7 @@ namespace RefreshTests.GameServer.GameServer;
 
 public class InMemoryGameDatabaseProvider : GameDatabaseProvider
 {
-    protected override string Filename => $"realm-inmemory-{Random.Shared.Next()}";
+    private readonly int _databaseId = Random.Shared.Next();
+    protected override string Filename => $"realm-inmemory-{this._databaseId}";
     protected override bool InMemory => true;
 }
