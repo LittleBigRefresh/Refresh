a57162bade657a9bcc99e2cad11b8b33d75f3338
jvyden <jvyden@jvyden.xyz>
2023-05-09 02:58:21 -0400

    More segmenting tests

--- RefreshTests.GameServer/Tests/ScoreLeaderboardTests.cs
+++ RefreshTests.GameServer/Tests/ScoreLeaderboardTests.cs
@@ -33,19 +33,19 @@ public class ScoreLeaderboardTests : GameServerTest
         List<GameSubmittedScore> scores = context.Database.GetTopScoresForLevel(level, 1, 0).ToList();
         Assert.That(scores, Has.Count.EqualTo(1));
     }
-
-    [Test]
-    public void PlacesScoreInSegmentCorrectly()
+    
+    /// <param name="count">The number of scores to try to fetch from the database</param>
+    /// <param name="expectedIndex">The expected index of the submitted score</param>
+    /// <param name="leaderboardCount">How many scores should be filled into the database</param>
+    /// <param name="submittedScore">The number of points the score has</param>
+    private void ScoreSegmentTest(int count, int expectedIndex, int leaderboardCount, int submittedScore)
     {
         TestContext context = this.GetServer();
         GameUser user = context.CreateUser();
         GameLevel level = context.CreateLevel(user);
 
-        context.FillLeaderboard(level, 25);
-        GameSubmittedScore score = context.SubmitScore(8, level, user);
-
-        const int count = 5;
-        const int expectedIndex = count / 2;
+        context.FillLeaderboard(level, leaderboardCount);
+        GameSubmittedScore score = context.SubmitScore(submittedScore, level, user);
 
         List<ObjectId> scores = context.Database.GetRankedScoresAroundScore(score, count)!
             .Select(s => s.score.ScoreId)
@@ -56,5 +56,21 @@ public class ScoreLeaderboardTests : GameServerTest
             Assert.That(scores.IndexOf(score.ScoreId), Is.EqualTo(expectedIndex));
             Assert.That(scores[expectedIndex], Is.EqualTo(score.ScoreId));
         });
+    }
+
+    [Test]
+    public void PlacesScoreInSegmentCorrectly()
+    {
+        const int count = 5;
+        const int expectedIndex = count / 2;
+
+        this.ScoreSegmentTest(count, expectedIndex, 25, 8);
+    }
+    
+    [Test]
+    public void SegmentsIfLessThanCountScore()
+    {
+        this.ScoreSegmentTest(5, 0, 4, 6);
+        this.ScoreSegmentTest(5, 2, 4, 1);
     }
 }
