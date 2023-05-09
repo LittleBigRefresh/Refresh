84fa24176f6429aa132302d694f56999254dd9b5
jvyden <jvyden@jvyden.xyz>
2023-05-08 21:49:15 -0400

    API authentication test

--- RefreshTests.GameServer/Tests/AuthenticationTests.cs
+++ RefreshTests.GameServer/Tests/AuthenticationTests.cs
@@ -1,11 +1,13 @@
 using Refresh.GameServer.Authentication;
+using Refresh.GameServer.Types.UserData;
+using RefreshTests.GameServer.Extensions;
 
 namespace RefreshTests.GameServer.Tests;
 
 public class AuthenticationTests : GameServerTest
 {
     [Test]
-    public async Task AuthenticationWorks()
+    public async Task GameAuthenticationWorks()
     {
         TestContext context = this.GetServer();
         
@@ -20,5 +22,30 @@ public class AuthenticationTests : GameServerTest
 
         HttpResponseMessage authedRequest = await authedClient.GetAsync("/lbp/playersInPodCount");
         Assert.That(authedRequest.StatusCode, Is.EqualTo(OK));
+    }
+    
+    [Test]
+    public async Task ApiAuthenticationWorks()
+    {
+        TestContext context = this.GetServer();
+        
+        HttpResponseMessage unauthedRequest = await context.Http.GetAsync("/api/v2/user/me");
+        Assert.That(unauthedRequest.StatusCode, Is.EqualTo(Forbidden));
+
+        HttpClient authedClient = context.GetAuthenticatedClient(TokenType.Api, out string tokenData);
+        
+        Token? token = context.Database.GetTokenFromTokenData(tokenData, TokenType.Api);
+        Assert.That(token, Is.Not.Null);
+        Assert.That(token?.User, Is.Not.Null);
+
+        // TODO: Fix serialization of ObjectId
+        HttpResponseMessage response = await authedClient.GetAsync("/api/v2/user/me");
+        // (GameUser? user, HttpResponseMessage response) = await authedClient.GetJsonObjectAsync<GameUser>("/api/v2/user/me");
+        Assert.Multiple(async () =>
+        {
+            // Assert.That(user, Is.Not.Null);
+            Assert.That(await response.Content.ReadAsStringAsync(), Contains.Substring(token!.User.UserId.ToString()));
+            Assert.That(response.StatusCode, Is.EqualTo(OK));
+        });
     }
 }
