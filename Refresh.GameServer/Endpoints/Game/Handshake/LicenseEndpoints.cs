5bc4568bdf81027c833d0c579e879c1b7448ee0a
jvyden <jvyden@jvyden.xyz>
2023-06-05 17:36:15 -0400

    Add AGPLv3 license to /eula

--- Refresh.GameServer/Endpoints/Game/Handshake/LicenseEndpoints.cs
+++ Refresh.GameServer/Endpoints/Game/Handshake/LicenseEndpoints.cs
@@ -6,14 +6,31 @@ namespace Refresh.GameServer.Endpoints.Game.Handshake;
 
 public class LicenseEndpoints : EndpointGroup
 {
+    private const string AGPLLicense = """
+    Copyright (C) 2023 LittleBigRefresh
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    """;
+    
     [GameEndpoint("eula")]
     public string License(RequestContext context, GameServerConfig config)
     {
-        return config.LicenseText;
+        return config.LicenseText + "\n\n" + AGPLLicense + "\n";
     }
 
     [GameEndpoint("announce")]
-    public string Announce(RequestContext context, GameServerConfig config) 
+    public string Announce(RequestContext context, GameServerConfig config)
     {
         return config.AnnounceText;
     }
