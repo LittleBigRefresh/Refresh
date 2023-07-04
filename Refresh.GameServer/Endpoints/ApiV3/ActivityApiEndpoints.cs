a34ac538730b1c392656a1e8651ee08db1ed4636
jvyden <jvyden@jvyden.xyz>
2023-07-03 21:12:42 -0400

    Hastily port ActivityApiEndpoints to ApiV3

---
+++ Refresh.GameServer/Endpoints/ApiV3/ActivityApiEndpoints.cs
@@ -0,0 +1,27 @@
+using Bunkum.HttpServer;
+using Bunkum.HttpServer.Endpoints;
+using Refresh.GameServer.Database;
+using Refresh.GameServer.Documentation.Attributes;
+using Refresh.GameServer.Endpoints.ApiV3.ApiTypes;
+using Refresh.GameServer.Types.Activity;
+
+namespace Refresh.GameServer.Endpoints.ApiV3;
+
+public class ActivityApiEndpoints : EndpointGroup
+{
+    // TODO: this api really needs improvement
+    // it's recent activity so i'm lazy
+    [ApiV3Endpoint("activity"), Authentication(false)]
+    [DocSummary("Fetch a list of recent happenings on the server.")]
+    [DocQueryParam("timestamp", "A timestamp in unix seconds, used to search backwards.")]
+    public ApiResponse<ActivityPage> GetRecentActivity(RequestContext context, GameDatabaseContext database)
+    {
+        long timestamp = 0;
+
+        string? tsStr = context.QueryString["timestamp"];
+        if (tsStr != null && !long.TryParse(tsStr, out timestamp)) return null;
+
+        ActivityPage page = new(database, generateGroups: false, timestamp: timestamp);
+        return page;
+    }
+}
