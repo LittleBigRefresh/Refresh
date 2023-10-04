77978355cc2762d518d691d9509611f268315ff0
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-10-03 20:26:04 -0700

    The Code That Got A Developer Arrested (LBP PSP team picks)

--- Refresh.GameServer/Endpoints/Game/ActivityEndpoints.cs
+++ Refresh.GameServer/Endpoints/Game/ActivityEndpoints.cs
@@ -1,9 +1,16 @@
+using System.Xml;
+using System.Xml.Serialization;
 using Bunkum.Core;
 using Bunkum.Core.Endpoints;
+using Bunkum.Core.Endpoints.Debugging;
+using Bunkum.Core.Responses;
 using Bunkum.Listener.Protocol;
+using Refresh.GameServer.Authentication;
 using Refresh.GameServer.Database;
+using Refresh.GameServer.Extensions;
 using Refresh.GameServer.Time;
 using Refresh.GameServer.Types.Activity;
+using Refresh.GameServer.Types.Levels;
 using Refresh.GameServer.Types.News;
 using Refresh.GameServer.Types.Roles;
 using Refresh.GameServer.Types.UserData;
@@ -34,44 +41,64 @@ public class ActivityEndpoints : EndpointGroup
     [GameEndpoint("news", ContentType.Xml)]
     [Authentication(false)]
     [MinimumRole(GameUserRole.Restricted)]
-    public GameNewsResponse GetNews(RequestContext context, IDateTimeProvider time)
+    [DebugResponseBody]
+    public Response GetNews(RequestContext context, GameDatabaseContext database, IDateTimeProvider time, Token? token)
     {
-        return new GameNewsResponse
+        List<GameNewsItem> items = new();
+
+        XmlSerializer itemContentSerializer = new(typeof(GameNewsItemContent));
+
+        if (token != null)
+        {
+            IEnumerable<GameLevel> teamPicks = database.GetTeamPickedLevels(32, 0, token.TokenGame).Items;
+            
+            int i = 0;
+            foreach (GameLevel level in teamPicks)
+            {
+                XmlDocument doc = new();
+
+                using (XmlWriter writer = doc.CreateNavigator()!.AppendChild())
+                {
+                    itemContentSerializer.Serialize(writer, new GameNewsItemContent
+                    {
+                        Frame = new GameNewsFrame
+                        {
+                            Width = 100,
+                            Title = "Team pick",
+                            Item = new GameNewsFrameItem
+                            {
+                                Width = 100,
+                                Content = "Team Pick",
+                                Background = "Frame item background",
+                                Level = new GameNewsFrameItemSlot
+                                {
+                                    Id = level.LevelId,
+                                    Type = "user",
+                                }
+                            }
+                        }
+                    });
+                }
+
+                items.Add(new GameNewsItem
+                {
+                    Id = i,
+                    Subject = "Team Pick",
+                    Content = doc.DocumentElement!.InnerXml,
+                });
+
+                i++;
+            }
+        }
+
+        return new Response(new GameNewsResponse
         {
             Subcategory = new GameNewsSubcategory
             {
                 Title = "Subcategory title",
                 Id = 1,
-                Item = new GameNewsItem
-                {
-                    Id = 2,
-                    Subject = "Item subject",
-                    Content = new GameNewsItemContent
-                    {
-                        Frame = new GameNewsFrame
-                        {
-                            Title = "Frame item title",
-                            Width = 1024,
-                            Item = new GameNewsFrameItem
-                            {
-                                Background = "Frame item background",
-                                Content = "Frame item content",
-                                Handle = new SerializedUserHandle
-                                {
-                                    Username = "Username",
-                                    IconHash = "0",
-                                },
-                                Level = new GameNewsFrameItemSlot
-                                {
-                                    Id = 0,
-                                    Type = "user",
-                                },
-                                Width = 1024,
-                            },
-                        },
-                    },
-                },
+                Items = items,
             },
-        };
+        }, ContentType.Xml);
     }
 }
--- Refresh.GameServer/Types/News/GameNewsItem.cs
+++ Refresh.GameServer/Types/News/GameNewsItem.cs
@@ -6,5 +6,5 @@ public class GameNewsItem
 {
     [XmlElement("id")] public required long Id { get; set; }
     [XmlElement("subject")] public required string Subject { get; set; }
-    [XmlElement("content")] public required GameNewsItemContent Content { get; set; }
+    [XmlElement("content")] public required string Content { get; set; }
 }
