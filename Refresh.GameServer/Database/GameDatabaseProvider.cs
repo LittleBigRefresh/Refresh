7e75e80ed98e229752c33c62f5fc1aa7d620f5df
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-10-16 00:09:35 -0700

    Add description to GameReport

--- Refresh.GameServer/Database/GameDatabaseProvider.cs
+++ Refresh.GameServer/Database/GameDatabaseProvider.cs
@@ -32,7 +32,7 @@ public class GameDatabaseProvider : RealmDatabaseProvider<GameDatabaseContext>
         this._time = time;
     }
 
-    protected override ulong SchemaVersion => 94;
+    protected override ulong SchemaVersion => 95;
 
     protected override string Filename => "refreshGameServer.realm";
     
