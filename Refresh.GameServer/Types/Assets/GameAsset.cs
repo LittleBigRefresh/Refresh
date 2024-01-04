97c495e88186a93909541945e9e650cd5fb0b541
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-01-04 12:52:23 -0800

    Update Refresh.GameServer/Types/Assets/GameAsset.cs
    Co-authored-by: Jayden <jvyden@jvyden.xyz>
    Signed-off-by: Beyley Thomas <ep1cm1n10n123@gmail.com>

--- Refresh.GameServer/Types/Assets/GameAsset.cs
+++ Refresh.GameServer/Types/Assets/GameAsset.cs
@@ -366,7 +366,7 @@ public partial class GameAsset : IRealmObject
             case GameAssetType.SyncedProfile:
             case GameAssetType.Unknown:
             default:
-                throw new ArgumentOutOfRangeException();
+                throw new InvalidOperationException($"Format '{this.AssetType}' is not a valid image.");
         }
     }
 }
