a3470db22d45445f1259ba4b70c211830aa99e91
jvyden <jvyden@jvyden.xyz>
2024-03-29 20:57:14 -0400

    Add server contact info to EULA

--- Refresh.GameServer/Endpoints/Game/Handshake/WelcomeEndpoints.cs
+++ Refresh.GameServer/Endpoints/Game/Handshake/WelcomeEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Bunkum.Core;
 using Bunkum.Core.Endpoints;
 using Refresh.GameServer.Configuration;
@@ -7,9 +8,8 @@ namespace Refresh.GameServer.Endpoints.Game.Handshake;
 
 public class WelcomeEndpoints : EndpointGroup
 {
-    private const string AGPLLicense = """
-    Copyright (C) 2023 LittleBigRefresh
-
+    private const string Copyright = "Copyright (C) 2023 LittleBigRefresh";
+    private const string AGPLNotice = """
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
@@ -26,8 +26,28 @@ public class WelcomeEndpoints : EndpointGroup
     
     [GameEndpoint("eula")]
     [MinimumRole(GameUserRole.Restricted)]
-    public string License(RequestContext context, GameServerConfig config)
+    public string License(RequestContext context, GameServerConfig config, ContactInfoConfig contact)
     {
-        return config.LicenseText + "\n\n" + AGPLLicense + "\n";
+        StringBuilder builder = new();
+        
+        builder.AppendLine(config.LicenseText);
+
+        builder.AppendLine();
+        builder.AppendLine($"{config.InstanceName} is operated by {contact.AdminName}, who you can email at {contact.EmailAddress}.");
+        if (contact.AdminDiscordUsername != null)
+            builder.AppendLine($"Alternatively, you can also contact {contact.AdminDiscordUsername} on Discord.");
+
+        if (contact.DiscordServerInvite != null)
+        {
+            builder.AppendLine();
+            builder.AppendLine($"We have a Discord server! Feel free to join here: {contact.DiscordServerInvite}");
+        }
+
+        builder.AppendLine(new string('=', Copyright.Length));
+        builder.AppendLine(Copyright);
+        builder.AppendLine();
+        builder.AppendLine(AGPLNotice);
+
+        return builder.ToString();
     }
 }
