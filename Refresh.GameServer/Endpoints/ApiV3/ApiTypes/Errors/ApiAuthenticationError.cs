a51d1d2e358e8799c50e94862f114d39db8b7374
uh wot <uhwot@protonmail.com>
2024-04-07 01:19:19 +0200

    fix ApiAuthenticationError typos

--- Refresh.GameServer/Endpoints/ApiV3/ApiTypes/Errors/ApiAuthenticationError.cs
+++ Refresh.GameServer/Endpoints/ApiV3/ApiTypes/Errors/ApiAuthenticationError.cs
@@ -3,10 +3,10 @@ namespace Refresh.GameServer.Endpoints.ApiV3.ApiTypes.Errors;
 [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public class ApiAuthenticationError : ApiError
 {
-    public const string NoPermissionsForObjectWhen = "You do not lack the permissions to manage or view this resource.";
+    public const string NoPermissionsForObjectWhen = "You lack the permissions to manage or view this resource.";
     public static readonly ApiAuthenticationError NoPermissionsForObject = new(NoPermissionsForObjectWhen);
     
-    public const string NoPermissionsForCreationWhen = "You do not lack the permissions to create this type of resource.";
+    public const string NoPermissionsForCreationWhen = "You lack the permissions to create this type of resource.";
     public static readonly ApiAuthenticationError NoPermissionsForCreation = new(NoPermissionsForCreationWhen);
     
     public bool Warning { get; init; }
