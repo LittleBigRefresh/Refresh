dd4606611b3090f7247c96b47442ca33a43c1db2
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-04-22 23:24:06 +0200

    Implement RateComment test

---
+++ RefreshTests.GameServer/Tests/Comments/CommentTests.cs
@@ -0,0 +1,63 @@
+﻿using Refresh.GameServer.Types.Comments;
+using Refresh.GameServer.Types.Lists;
+using Refresh.GameServer.Types.Reviews;
+using Refresh.GameServer.Types.UserData;
+using RefreshTests.GameServer.Extensions;
+using TokenType = Refresh.GameServer.Authentication.TokenType;
+
+namespace RefreshTests.GameServer.Tests.Comments;
+
+public class CommentTests : GameServerTest
+{
+    [Test]
+    public void RateComment()
+    {
+        using TestContext context = this.GetServer();
+        GameUser user = context.CreateUser();
+
+        using HttpClient client = context.GetAuthenticatedClient(TokenType.Game, user);
+        GameComment comment = context.Database.PostCommentToProfile(user, user, "This is a test comment!");
+
+        foreach (RatingType ratingType in new List<RatingType> {RatingType.Neutral, RatingType.Boo, RatingType.Yay})
+        {
+            for (int i = 0; i < 3; i++) // Rate multiple times to test that duplicate ratings are not added
+            {
+                // ReSharper disable once RedundantAssignment
+                client.PostAsync($"/lbp/rateUserComment/{user.Username}?commentId={comment.SequentialId}&rating={ratingType.ToDPad()}", null);
+            }
+        
+            HttpResponseMessage response = client.GetAsync($"/lbp/userComments/{user.Username}").Result;
+            SerializedCommentList userComments = response.Content.ReadAsXML<SerializedCommentList>();
+            comment = userComments.Items.First();
+
+            int expectedThumbsUp, expectedThumbsDown;
+            
+            switch (ratingType)
+            {
+                case RatingType.Neutral:
+                    expectedThumbsDown = 0;
+                    expectedThumbsUp = 0;
+                    break;
+                case RatingType.Boo:
+                    expectedThumbsDown = 1;
+                    expectedThumbsUp = 0;
+                    break;
+                case RatingType.Yay:
+                    expectedThumbsDown = 0;
+                    expectedThumbsUp = 1;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(comment.YourThumb, Is.EqualTo(ratingType.ToDPad()));
+                Assert.That(comment.ThumbsUp, Is.EqualTo(expectedThumbsUp));
+                Assert.That(comment.ThumbsDown, Is.EqualTo(expectedThumbsDown));
+            });
+        }
+        
+        
+    }
+}
