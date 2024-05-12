88edbf90cdfa83a4fa7992187ba98ae1905da198
jvyden <jvyden@jvyden.xyz>
2024-05-12 19:10:06 -0400

    Revise registration queue warning

--- Refresh.GameServer/Endpoints/ApiV3/AuthenticationApiEndpoints.cs
+++ Refresh.GameServer/Endpoints/ApiV3/AuthenticationApiEndpoints.cs
@@ -263,8 +263,8 @@ public class AuthenticationApiEndpoints : EndpointGroup
             database.AddRegistrationToQueue(body.Username, body.EmailAddress, passwordBcrypt);
             return new ApiAuthenticationError(
                 "Your account has been put into the registration queue, but it is not yet activated. " +
-                "To complete registration, simply log in from LBP within the next hour and your new account will be activated. " +
-                "You will be unable to sign in until you are patched and playing.", true);
+                "To complete registration, patch your games to our servers and start playing within the next hour and your new account will be activated. " +
+                "You will be unable to sign in until you are patched and playing. For more instructions on patching, please visit https://docs.littlebigrefresh.com", true);
         }
 
         GameUser user = database.CreateUser(body.Username, body.EmailAddress, true);
