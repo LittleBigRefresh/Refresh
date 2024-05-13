648d8e4e73a2a0186ac2ebf306de41b8b4e766dd
jvyden <jvyden@jvyden.xyz>
2024-05-13 16:52:44 -0400

    Add ability to match usernames case insensitively

--- Refresh.GameServer/Database/GameDatabaseContext.Levels.cs
+++ Refresh.GameServer/Database/GameDatabaseContext.Levels.cs
@@ -348,7 +348,7 @@ public partial class GameDatabaseContext // Levels
         }
         
         // Try to look up a username to search by publisher.
-        GameUser? publisher = this.GetUserByUsername(query); 
+        GameUser? publisher = this.GetUserByUsername(query, false); 
         if (publisher != null)
         {
             levels.AddRange(validLevels.Where(l => l.Publisher == publisher));
