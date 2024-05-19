bf4c63ce7073634e0f459260a5fbc7b8d93e44f7
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-05-19 16:38:19 -0700

    Port BEBinaryReader from GenderFluid

--- Refresh.GameServer/Importing/BEBinaryReader.cs
+++ Refresh.GameServer/Importing/BEBinaryReader.cs
@@ -7,11 +7,41 @@ public class BEBinaryReader : BinaryReader
     public BEBinaryReader(Stream input) : base(input)
     {}
     
-    public override short ReadInt16() => BinaryPrimitives.ReadInt16BigEndian(this.ReadBytes(2));
-    public override int ReadInt32() => BinaryPrimitives.ReadInt32BigEndian(this.ReadBytes(4));
-    public override long ReadInt64() => BinaryPrimitives.ReadInt64BigEndian(this.ReadBytes(8));
-
-    public override ushort ReadUInt16() => BinaryPrimitives.ReadUInt16BigEndian(this.ReadBytes(2));
-    public override uint ReadUInt32() => BinaryPrimitives.ReadUInt32BigEndian(this.ReadBytes(4));
-    public override ulong ReadUInt64() => BinaryPrimitives.ReadUInt64BigEndian(this.ReadBytes(8));
+    public override short ReadInt16()
+    {
+        Span<byte> buf = stackalloc byte[sizeof(short)];
+        this.BaseStream.ReadExactly(buf);
+        return BinaryPrimitives.ReadInt16BigEndian(buf);
+    }
+    public override int ReadInt32()
+    {
+        Span<byte> buf = stackalloc byte[sizeof(int)];
+        this.BaseStream.ReadExactly(buf);
+        return BinaryPrimitives.ReadInt32BigEndian(buf);
+    }
+    public override long ReadInt64()
+    {
+        Span<byte> buf = stackalloc byte[sizeof(long)];
+        this.BaseStream.ReadExactly(buf);
+        return BinaryPrimitives.ReadInt64BigEndian(buf);
+    }
+    
+    public override ushort ReadUInt16()
+    {
+        Span<byte> buf = stackalloc byte[sizeof(ushort)];
+        this.BaseStream.ReadExactly(buf);
+        return BinaryPrimitives.ReadUInt16BigEndian(buf);
+    }
+    public override uint ReadUInt32()
+    {
+        Span<byte> buf = stackalloc byte[sizeof(uint)];
+        this.BaseStream.ReadExactly(buf);
+        return BinaryPrimitives.ReadUInt32BigEndian(buf);
+    }
+    public override ulong ReadUInt64()
+    {
+        Span<byte> buf = stackalloc byte[sizeof(ulong)];
+        this.BaseStream.ReadExactly(buf);
+        return BinaryPrimitives.ReadUInt64BigEndian(buf);
+    }
 }
