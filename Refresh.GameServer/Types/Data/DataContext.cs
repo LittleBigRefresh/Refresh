30858f26e1adb528272845e2f7674b3551009df0
jvyden <jvyden@jvyden.xyz>
2024-08-11 16:34:08 -0400

    Add easy shortcut for getting an icon

--- Refresh.GameServer/Types/Data/DataContext.cs
+++ Refresh.GameServer/Types/Data/DataContext.cs
@@ -19,4 +19,9 @@ public class DataContext
     public GameUser? User => this.Token?.User;
     public TokenGame Game => this.Token?.TokenGame ?? TokenGame.Website;
     public TokenPlatform Platform => this.Token?.TokenPlatform ?? TokenPlatform.Website;
+
+    public string GetIconFromHash(string hash)
+    {
+        return this.Database.GetAssetFromHash(hash)?.GetAsIcon(this.Game, this) ?? hash;
+    }
 }
