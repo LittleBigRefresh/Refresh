293b4d5d8389579e9da48bcf4ee85c900f3bebc5
jvyden <jvyden@jvyden.xyz>
2025-03-08 16:25:53 -0500

    Fix build errors

--- Refresh.Common/Verification/LbpSigningKey.cs
+++ Refresh.Common/Verification/LbpSigningKey.cs
@@ -7,6 +7,6 @@ public class LbpSigningKey : PsnSigningKey
     public static readonly LbpSigningKey Instance = new();
     private LbpSigningKey() {}
     
-    public override string CurveX => "39c62d061d4ee35c5f3f7531de0af3cf918346526edac727";
-    public override string CurveY => "a5d578b55113e612bf1878d4cc939d61a41318403b5bdf86";
+    public override string PublicKeyX => "39c62d061d4ee35c5f3f7531de0af3cf918346526edac727";
+    public override string PublicKeyY => "a5d578b55113e612bf1878d4cc939d61a41318403b5bdf86";
 }
