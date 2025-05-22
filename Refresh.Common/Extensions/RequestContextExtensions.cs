47567822b17f71d9bab932f7427afdb2632aa750
jvyden <jvyden@jvyden.xyz>
2025-05-22 18:55:07 -0400

    Add validation for patchwork useragent

--- Refresh.Common/Extensions/RequestContextExtensions.cs
+++ Refresh.Common/Extensions/RequestContextExtensions.cs
@@ -27,6 +27,43 @@ public static class RequestContextExtensions
 
     [Pure]
     public static bool IsApi(this RequestContext context) => context.Url.AbsolutePath.StartsWith("/api/");
+
+    [Pure]
+    public static bool IsPatchworkVersionValid(this RequestContext context, int requiredMajor, int requiredMinor)
+    {
+        ReadOnlySpan<char> userAgent = context.RequestHeaders.Get("User-Agent").AsSpan();
+        if (userAgent.IsEmpty)
+            return false;
+
+        return IsPatchworkVersionValid(userAgent, requiredMajor, requiredMinor);
+    }
+
+    [Pure]
+    public static bool IsPatchworkVersionValid(ReadOnlySpan<char> userAgent, int requiredMajor, int requiredMinor)
+    {
+        // example: PatchworkLBP2 1.0
+        const string namePrefix = "PatchworkLBP";
+
+        // does the useragent even match patchwork's name?
+        if (!userAgent.StartsWith(namePrefix))
+            return false;
+        
+        // is the game version valid?
+        char gameVersion = userAgent[namePrefix.Length];
+        if (gameVersion is not '1' and not '2' and not '3' and not 'V')
+            return false;
+
+        // does the version string parse out?
+        if (!Version.TryParse(userAgent[(namePrefix.Length + 2)..], out Version? version))
+            return false;
+
+        // are we on a supported version?
+        if (version.Major < requiredMajor || version.Minor < requiredMinor)
+            return false;
+
+        // if everything held up, this client is up to date on security patches!
+        return true;
+    }
     
     [Pure]
     public static string RemoteIp(this RequestContext context) => ((IPEndPoint)context.RemoteEndpoint).Address.ToString();
