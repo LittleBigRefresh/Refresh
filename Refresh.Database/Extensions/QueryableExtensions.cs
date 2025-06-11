9024fe66a7cccf4fd01703a1038d9863b7707279
jvyden <jvyden@jvyden.xyz>
2025-06-11 19:49:36 -0400

    Add Realm polyfill for IQueryable<T>.Include

---
+++ Refresh.Database/Extensions/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Refresh.Database.Extensions;
+
+public static class QueryableExtensions
+{
+#if !POSTGRES
+    public static IQueryable<T> Include<T>(this IQueryable<T> queryable, Func<T, object> func)
+    {
+        _ = func;
+        return queryable;
+    }
+#endif
+}
