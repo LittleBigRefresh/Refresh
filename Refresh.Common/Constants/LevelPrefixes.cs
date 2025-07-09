1f308a4e0fae4096950e784e6b95b73169925760
Nya0 <Nya0@duck.com>
2025-07-09 16:02:54 +0300

    new method to extract attributes into dictionary

--- Refresh.Common/Constants/LevelPrefixes.cs
+++ Refresh.Common/Constants/LevelPrefixes.cs
@@ -4,8 +4,24 @@ namespace Refresh.Common.Constants;
 
 public static partial class LevelPrefixes
 {
+    
+    /// <summary>
+    /// Matches values of ?key:value or ?key.value
+    /// </summary>
     [GeneratedRegex(@"\?(\w+)[\.:]([\w-]+)")]
-    public static partial Regex AttributeRegex();
+    private static partial Regex AttributeRegex();
+    
+    /// <summary>
+    /// Extracts matches from AttributeRegex into a dictionary 
+    /// </summary>
+    public static Dictionary<string, string> ExtractAttributes(string description)
+    {
+        MatchCollection matches = LevelPrefixes.AttributeRegex().Matches(description);
+        return matches.ToDictionary(
+            m => m.Groups[1].Value, 
+            m => m.Groups[2].Value
+        );
+    }
     
     /// <summary>
     /// Keywords that indicate a level is a reupload
