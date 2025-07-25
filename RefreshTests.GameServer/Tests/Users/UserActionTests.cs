f1c1ed711d4c7bdcf3adf181a5eadfc6e7175d7a
jvyden <jvyden@jvyden.xyz>
2025-07-25 16:02:32 -0400

    Add test case for renaming a user

---
+++ RefreshTests.GameServer/Tests/Users/UserActionTests.cs
@@ -0,0 +1,21 @@
+﻿using Refresh.Database.Models.Users;
+
+namespace RefreshTests.GameServer.Tests.Users;
+
+public class UserActionTests : GameServerTest
+{
+    [Test]
+    public void RenamesUser()
+    {
+        using TestContext context = this.GetServer(false);
+        GameUser? user = context.CreateUser("gamer1");
+        
+        Assert.That(user.Username, Is.EqualTo("gamer1"));
+        
+        context.Database.RenameUser(user, "gamer2");
+        user = context.Database.GetUserByObjectId(user.UserId);
+        Assert.That(user, Is.Not.Null);
+        
+        Assert.That(user.Username, Is.EqualTo("gamer2"));
+    }
+}
