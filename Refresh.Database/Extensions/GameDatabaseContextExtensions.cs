1c826cfeae0fed16819d3004e9ff8dd677f1541a
Toaster2 <brottoastlemon@gmail.com>
2025-10-20 18:05:02 +0200

    Add backfill migration to track modded status of already uploaded planets

--- Refresh.Database/Extensions/GameDatabaseContextExtensions.cs
+++ Refresh.Database/Extensions/GameDatabaseContextExtensions.cs
@@ -1,9 +1,18 @@
 ﻿using Refresh.Database.Models.Assets;
+using Refresh.Database.Models.Users;
 
 namespace Refresh.Database.Extensions;
 
 public static class GameDatabaseContextExtensions
 {
+    public static void UpdatePlanetModdedStatus(this GameDatabaseContext database, GameUser user)
+    {
+        user.AreLbp2PlanetsModded = database.GetPlanetModdedStatus(user.Lbp2PlanetsHash);
+        user.AreLbp3PlanetsModded = database.GetPlanetModdedStatus(user.Lbp3PlanetsHash);
+        user.AreVitaPlanetsModded = database.GetPlanetModdedStatus(user.VitaPlanetsHash);
+        user.AreBetaPlanetsModded = database.GetPlanetModdedStatus(user.BetaPlanetsHash);
+    }
+
     public static bool GetPlanetModdedStatus(this GameDatabaseContext database, string rootAssetHash)
     {
         bool modded = false;
--- Refresh.Interfaces.Workers/RefreshWorkerManager.cs
+++ Refresh.Interfaces.Workers/RefreshWorkerManager.cs
@@ -24,6 +24,7 @@ public static class RefreshWorkerManager
         manager.AddJob<BackfillLevelAttributesMigration>();
         manager.AddJob<BackfillReviewLabelsMigration>();
         manager.AddJob<ClampPlayerLimitsMigration>();
+        manager.AddJob<BackfillModdedPlanetFlagsMigration>();
         
         return manager;
     }
