ed4fdaae3f663f5b93505091ac66e709ede46e3a
Toaster2 <brottoastlemon@gmail.com>
2025-11-08 13:17:35 +0100

    Correct comment

--- Refresh.Interfaces.Game/Constants/PlaylistEndpointLimits.cs
+++ Refresh.Interfaces.Game/Constants/PlaylistEndpointLimits.cs
@@ -4,7 +4,7 @@ public static class PlaylistEndpointLimits
 {
     // rate-limits
     public const int UploadTimeoutDuration = 300;
-    public const int MaxCreateAmount = 4; // should be more than enough
+    public const int MaxCreateAmount = 4; // should be enough
     public const int MaxUpdateAmount = 12;
     public const int UploadBlockDuration = 300;
     public const string CreateBucket = "playlist-create";
