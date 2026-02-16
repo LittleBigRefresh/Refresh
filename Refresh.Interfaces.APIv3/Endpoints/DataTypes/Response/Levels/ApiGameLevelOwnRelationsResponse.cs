eb14e3a504a787e3458a14df1f38fd485f587c4b
ToasterTheBrot <95178340+Toastbrot236@users.noreply.github.com>
2026-02-16 13:52:10 +0100

    Better MyPlaysCount summary
    Signed-off-by: ToasterTheBrot <95178340+Toastbrot236@users.noreply.github.com>

--- Refresh.Interfaces.APIv3/Endpoints/DataTypes/Response/Levels/ApiGameLevelOwnRelationsResponse.cs
+++ Refresh.Interfaces.APIv3/Endpoints/DataTypes/Response/Levels/ApiGameLevelOwnRelationsResponse.cs
@@ -11,8 +11,7 @@ public class ApiGameLevelOwnRelationsResponse : IApiResponse
     public required int LevelRating { get; set; }
 
     /// <summary>
-    /// Returns the total amount of plays; ambiguous name because a unique play count would be nonsense
-    /// because this is only about one user. Probably rename this in APIv4 anyway.
+    /// Returns the total amount of plays. Probably rename this in APIv4 for clarity.
     /// </summary>
     public required int MyPlaysCount { get; set; }
     public required int CompletionCount { get; set; }
