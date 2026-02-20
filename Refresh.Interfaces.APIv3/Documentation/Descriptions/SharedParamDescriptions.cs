b83ea0651c16bf9f6b47114bce17ca614d45ff8d
Toaster2 <brottoastlemon@gmail.com>
2026-02-20 13:56:19 +0100

    Add SharedParamDescriptions

---
+++ Refresh.Interfaces.APIv3/Documentation/Descriptions/SharedParamDescriptions.cs
@@ -0,0 +1,7 @@
+namespace Refresh.Interfaces.APIv3.Documentation.Descriptions;
+
+public static class SharedParamDescriptions
+{
+    public const string UserIdParam = "The UUID or (user)name of the user.";
+    public const string UserIdTypeParam = "The type of ID used to specify the user. Can be 'uuid', 'username' or 'name'.";
+}
