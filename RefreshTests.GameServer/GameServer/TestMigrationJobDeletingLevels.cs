2504ec1c98bc80528294491e7237522292f087d1
Toaster2 <brottoastlemon@gmail.com>
2026-03-30 17:07:03 +0200

    Test entity deletion during migration

---
+++ RefreshTests.GameServer/GameServer/TestMigrationJobDeletingLevels.cs
@@ -0,0 +1,37 @@
+﻿using Refresh.Database.Models.Levels;
+using Refresh.Workers;
+
+namespace RefreshTests.GameServer.GameServer;
+
+public class TestMigrationJobDeletingLevels : MigrationJob<GameLevel>
+{
+    protected override int BatchCount => 6;
+
+    // Deletes every second level
+    protected override int Migrate(WorkContext context, GameLevel[] batch)
+    {
+        int entitiesLeft = batch.Length;
+        int index = 0;
+        foreach (GameLevel level in batch)
+        {
+            if (index % 2 == 1)
+            {
+                context.Database.DeleteLevel(level);
+                entitiesLeft--;
+            }
+            else
+            {
+                level.Title += " test";
+            }
+
+            index++;
+        }
+
+        return entitiesLeft;
+    }
+
+    protected override IQueryable<GameLevel> SortAndFilter(IQueryable<GameLevel> query)
+    {
+        return query.OrderBy(l => l.LevelId);
+    }
+}
