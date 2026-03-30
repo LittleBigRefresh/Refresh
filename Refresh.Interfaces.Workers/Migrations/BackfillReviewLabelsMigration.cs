5bf1dcd097b497e345b9e2c0bcb708f587f75452
Toaster2 <brottoastlemon@gmail.com>
2026-03-30 13:53:01 +0200

    Move review label migration out of Refresh.Database

--- Refresh.Interfaces.Workers/Migrations/BackfillReviewLabelsMigration.cs
+++ Refresh.Interfaces.Workers/Migrations/BackfillReviewLabelsMigration.cs
@@ -1,4 +1,5 @@
-﻿using Refresh.Database.Models.Comments; 
+﻿using Refresh.Database.Models.Comments;
+using Refresh.Database.Models.Levels;
 using Refresh.Workers;
 
 namespace Refresh.Interfaces.Workers.Migrations;
@@ -7,7 +8,17 @@ public class BackfillReviewLabelsMigration : MigrationJob<GameReview>
 {
     protected override void Migrate(WorkContext context, GameReview[] batch)
     {
-        context.Database.MigrateReviewLabels(batch);
+        foreach (GameReview review in batch)
+        {
+#pragma warning disable CS0618 // LabelsString is obsolete          
+            if (string.IsNullOrWhiteSpace(review.LabelsString)) continue;
+
+            context.Database.Update(review);
+            review.Labels = LabelExtensions.FromLbpCommaList(review.LabelsString).ToList();
+#pragma warning restore CS0618
+        }
+
+        context.Database.SaveChanges();
     }
 
     protected override IQueryable<GameReview> SortAndFilter(IQueryable<GameReview> query)
