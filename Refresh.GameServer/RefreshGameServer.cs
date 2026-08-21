bd65b7da287e59a37044a117f357b6118dcce166
Toaster2 <brottoastlemon@gmail.com>
2026-08-21 15:28:19 +0200

    Update GameRateLimitService initialization in RefreshGameServer

--- Refresh.GameServer/RefreshGameServer.cs
+++ Refresh.GameServer/RefreshGameServer.cs
@@ -142,7 +142,7 @@ public class RefreshGameServer : RefreshServer
     protected override void SetupServices()
     {
         this.Server.AddService<TimeProviderService>(this.GetTimeProvider());
-        this.Server.AddService<GameRateLimitService>(new RateLimiter(new RateLimitSettings(90, 380, 45, "global")));
+        this.Server.AddService<GameRateLimitService>();
         this.Server.AddService<CategoryService>();
         this.Server.AddService(new MatchService(this.Server.Logger, this._configStore.GameServer));
         this.Server.AddService<ImportService>();
